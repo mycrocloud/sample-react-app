034ffbaa915877894bd5d24307944d4dc7d73bf4
Nam Pham <pvnam95@hotmail.com>
2024-11-08 14:08:26 +0900

    Deployed on MycroCloud (2024/11/08 14:00)

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Deployed on MycroCloud (2024/10/29 00:54)</p>
+      <p>Deployed on MycroCloud (2024/11/08 14:08)</p>
     </>
   );
 }
